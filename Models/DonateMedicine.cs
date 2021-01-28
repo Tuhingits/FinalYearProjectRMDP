68771bb1d2d80a69f4c76a70d1f21e3f53b6278c
Tuhin <tuhin35-1947@diu.edu.bd>
2021-01-28 22:31:10 +0600

    initial

---
+++ Models/DonateMedicine.cs
@@ -0,0 +1,26 @@
+﻿using MemberShip.Entities;
+using MemberShip.Services;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reduntant_Medicine_Donation_portal.Models
+{
+    public class DonateMedicine
+    {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public int Quantity { get; set; }
+        public string DonarId { get; set; }
+        public int OrgId { get; set; }
+
+        //public IList<Organizer> Organizer { get; set; }
+        //public IList<ApplicationUser> ApplicationUser { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac.Extensions.DependencyInjection;
+
+namespace Reduntant_Medicine_Donation_portal
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+           Host.CreateDefaultBuilder(args)
+               .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+               .ConfigureWebHostDefaults(webBuilder =>
+               {
+                   webBuilder.UseStartup<Startup>();
+               });
+    }
+}
